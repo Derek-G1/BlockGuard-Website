61e26e27f32bad8b3ec5ea85ab09d66343ffd243
Derek-G1 <Derek_Gembus@yahoo.com>
2025-05-21 16:46:38 -0400

    removed text of support email from page to prevent bots from spamming the support email

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ const pages = {
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
                     Email Support
                 </a>
-                <p class="mt-2 text-xs text-gray-500 dark:text-gray-400">(support@blockguard.app)</p>
+                
             </div>
         </div>
     `,
