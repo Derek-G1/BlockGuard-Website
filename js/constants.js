1cf61048d68f0b58313e18fc8bcf23d94ea192f6
Derek-G1 <Derek_Gembus@yahoo.com>
2025-12-27 11:10:24 -0500

    Updated Privacy Policy, bumped version to 1.2.6, and fixed HTML typo

---
+++ js/constants.js
@@ -0,0 +1,5 @@
+// constants.js
+
+export const APP_VERSION = "1.2.6";
+export const PLAY_STORE_LINK = "https://play.google.com/store/apps/details?id=com.radstormtech.blockguard";
+export const MIN_ANDROID_VERSION = "Android 9 Pie (API 28)";
