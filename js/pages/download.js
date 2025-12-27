f5c87540c6a3276fd002a773022ed2a3b1586967
Derek-G1 <Derek_Gembus@yahoo.com>
2025-12-27 15:14:33 -0500

    updated version number on download page

--- js/pages/download.js
+++ js/pages/download.js
@@ -1,7 +1,7 @@
 // js/pages/download.js
 
 // Constants
-const APP_VERSION = "1.1.0";
+const APP_VERSION = "1.2.6";
 const PLAY_STORE_LINK = "https://play.google.com/store/apps/details?id=com.radstormtech.blockguard";
 const MIN_ANDROID_VERSION = "Android 9 Pie (API 28)";
 
