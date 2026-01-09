6922eaeeebd395b288144d5a964e96648bddef34
Derek-G1 <Derek_Gembus@yahoo.com>
2026-01-08 20:55:29 -0500

    commit after running: npm run build:css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
-    content: ["./**/*.{html,js}"],
+    content: [
+        "./index.html",
+        "./js/**/*.js",
+        "./src/**/*.{html,js}"
+    ],
     theme: {
         extend: {
             fontFamily: {
